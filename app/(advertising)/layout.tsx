dea66edc5207a6c0e2208206a990ccd29375c8b7
BrunoReCarmo <bruno.reis.carmo@gmail.com>
2024-01-16 16:27:39 -0300

    Landing Page (Starting structure) [#4]

---
+++ app/(advertising)/layout.tsx
@@ -0,0 +1,15 @@
+const AdvertisingLayout = ({
+    children
+}: {
+    children: React.ReactNode;
+}) => {
+    return (
+        <div className="h-full bg-slate-100">
+            <main className="pt-40 pb-20 bg-slate-100">
+            {children}
+            </main>
+        </div>
+    );
+};
+
+export default AdvertisingLayout;
