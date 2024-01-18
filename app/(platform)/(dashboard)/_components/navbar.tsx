45fe5d65be096bbd11d28c1d3b95db5a8d8ab2ea
BrunoReCarmo <bruno.reis.carmo@gmail.com>
2024-01-18 15:30:07 -0300

    Sidebar (Mobile and Desktop) [#10]

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -1,12 +1,13 @@
 import { Logo } from "@/components/logo"
 import { Button } from "@/components/ui/button"
+import { MobileSidebar } from "./mob-sidebar"
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs"
 import { Plus } from "lucide-react"
 
 export const Navbar = () => {
     return (
         <nav className="fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center">
-            {/*Mobile Sidebar*/}
+            <MobileSidebar />
             <div className="flex items-center gap-x-4">
                 <div className="hidden md:flex">
                     <Logo />
---
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import Link from "next/link";
+import { Plus } from "lucide-react";
+import { useLocalStorage } from "usehooks-ts";
+import { useOrganization, useOrganizationList } from "@clerk/nextjs";
+
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Accordion } from "@/components/ui/accordion";
+
+import { NavItem, Organization } from "./nav-item";
+
+interface SidebarProps {
+    storageKey?: string;
+};
+
+export const Sidebar = ({ storageKey = "t-sidebar-state", }: SidebarProps) => {
+    const [opened, SetOpened] = useLocalStorage<Record<string, any>>
+        (storageKey,
+            {}
+        );
+
+    const {
+        organization: activeOrganization,
+        isLoaded: isLoadedOrganization } = useOrganization();
+
+    const {
+        userMemberships,
+        isLoaded: isLoadedOrganizationList
+    } = useOrganizationList({
+        userMemberships: {
+            infinite: true,
+        },
+    });
+
+    const defaultAccordionValue: string[] = Object.keys(opened)
+        .reduce((acc: string[], key: string) => {
+            if (opened[key]) {
+                acc.push(key);
+            }
+
+            return acc;
+        }, []);
+
+    const onOpen = (id: string) => {
+        SetOpened((curr) => ({
+            ...curr,
+            [id]: !opened[id],
+        }));
+    };
+
+    if (!isLoadedOrganization || !isLoadedOrganizationList || userMemberships.isLoading) {
+        return (
+            <>
+                <Skeleton />
+            </>
+        )
+    }
+
+    return (
+        <>
+            <div className="font-medium text-xs flex items-center mb-1">
+                <span className="pl-4">
+                    Workspaces
+                </span>
+                <Button
+                    asChild
+                    type="button"
+                    size="icon"
+                    variant="ghost"
+                    className="ml-auto">
+                    <Link href="/select-org">
+                        <Plus
+                            className="h-4 w-4" />
+                    </Link>
+                </Button>
+            </div>
+            <Accordion
+                type="multiple"
+                defaultValue={defaultAccordionValue}
+                className="space-y-2"
+            >
+                {userMemberships.data.map(({ organization }) => (
+                    <NavItem
+                        key={organization.id}
+                        isActive={activeOrganization?.id === organization.id}
+                        isOpened={opened[organization.id]}
+                        organization={organization as Organization}
+                        onOpen={onOpen}
+                    />
+                ))}
+            </Accordion>
+        </>
+    );
+};
