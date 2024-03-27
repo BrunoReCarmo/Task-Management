a6b2d082f318c4c6caca4c03c065eea2aeeccb2a
bruno.reis.carmo@gmail.com <bruno.reis.carmo@gmail.com>
2024-03-27 14:25:23 -0300

    ORM Prisma connect to Supabase (App => DB) [#24]

--- app/(platform)/(dashboard)/organization/[OrganizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[OrganizationId]/page.tsx
@@ -1,9 +1,29 @@
+import { db } from "@/lib/db";
 
 const OrganizationIdPage = () => {
+    async function create(formData: FormData) {
+        "use server";
+
+        const title = formData.get("title") as string;
+
+        await db.board.create({
+            data: {
+                title,
+            }
+        });
+    }
 
     return (
         <div>
-            Organization Page
+            <form action={create}>
+                <input
+                    id="title"
+                    name="title"
+                    required
+                    placeholder="Insira um título para seu quadro"
+                    className="border-black border p-1 rounded-md"
+                />
+            </form>
         </div>
     );
 };
